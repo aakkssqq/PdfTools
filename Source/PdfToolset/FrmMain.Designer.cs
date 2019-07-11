4e16871cc9c85d04ccbd949bf6a09a74af23d4ff
iimax <imaxis@163.com>
2019-07-11 18:37:49 +0800

    pdftoolset winform
    batch remove the last page

---
+++ Source/PdfToolset/FrmMain.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace PdfToolset
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.btnOpenFolder = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.btnChooseFile = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.btnExecute = new System.Windows.Forms.ToolStripButton();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.lstFiles = new System.Windows.Forms.ListBox();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.btnClear = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnOpenFolder,
+            this.toolStripSeparator1,
+            this.btnChooseFile,
+            this.toolStripSeparator2,
+            this.btnClear,
+            this.toolStripSeparator3,
+            this.btnExecute});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // btnOpenFolder
+            // 
+            this.btnOpenFolder.Image = ((System.Drawing.Image)(resources.GetObject("btnOpenFolder.Image")));
+            this.btnOpenFolder.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnOpenFolder.Name = "btnOpenFolder";
+            this.btnOpenFolder.Size = new System.Drawing.Size(85, 22);
+            this.btnOpenFolder.Text = "选文件夹...";
+            this.btnOpenFolder.Click += new System.EventHandler(this.btnOpenFolder_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // btnChooseFile
+            // 
+            this.btnChooseFile.Image = ((System.Drawing.Image)(resources.GetObject("btnChooseFile.Image")));
+            this.btnChooseFile.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnChooseFile.Name = "btnChooseFile";
+            this.btnChooseFile.Size = new System.Drawing.Size(71, 22);
+            this.btnChooseFile.Text = "选PDF...";
+            this.btnChooseFile.Click += new System.EventHandler(this.btnChooseFile_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 25);
+            // 
+            // btnExecute
+            // 
+            this.btnExecute.Image = ((System.Drawing.Image)(resources.GetObject("btnExecute.Image")));
+            this.btnExecute.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnExecute.Name = "btnExecute";
+            this.btnExecute.Size = new System.Drawing.Size(100, 22);
+            this.btnExecute.Text = "删除最后一页";
+            this.btnExecute.Click += new System.EventHandler(this.btnExecute_Click);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 25);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.txtLog);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 425);
+            this.splitContainer1.SplitterDistance = 266;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // lstFiles
+            // 
+            this.lstFiles.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lstFiles.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lstFiles.FormattingEnabled = true;
+            this.lstFiles.ItemHeight = 21;
+            this.lstFiles.Location = new System.Drawing.Point(3, 17);
+            this.lstFiles.Name = "lstFiles";
+            this.lstFiles.Size = new System.Drawing.Size(794, 246);
+            this.lstFiles.TabIndex = 0;
+            // 
+            // txtLog
+            // 
+            this.txtLog.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtLog.Location = new System.Drawing.Point(0, 0);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.Size = new System.Drawing.Size(800, 155);
+            this.txtLog.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lstFiles);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(800, 266);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "待处理PDF列表：";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "PDF文件|*.pdf";
+            // 
+            // toolStripSeparator3
+            // 
+            this.toolStripSeparator3.Name = "toolStripSeparator3";
+            this.toolStripSeparator3.Size = new System.Drawing.Size(6, 25);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Image = ((System.Drawing.Image)(resources.GetObject("btnClear.Image")));
+            this.btnClear.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(76, 22);
+            this.btnClear.Text = "清空列表";
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "FrmMain";
+            this.Text = "批量删除PDF最后一页";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton btnOpenFolder;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripButton btnChooseFile;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripButton btnExecute;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ListBox lstFiles;
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+        private System.Windows.Forms.ToolStripButton btnClear;
+    }
+}
+
---
+++ Source/PdfToolset/FrmMain.cs
@@ -0,0 +1,189 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PdfToolset
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private List<string> lstPDFFiles = new List<string>();
+
+        private void AppendFile(string path, string filename)
+        {
+            if (lstPDFFiles.Contains(path))
+            {
+                return;
+            }
+            lstPDFFiles.Add(path);
+            this.lstFiles.Items.Add(filename);
+            
+        }
+
+        private void btnOpenFolder_Click(object sender, EventArgs e)
+        {
+            var dr = this.folderBrowserDialog1.ShowDialog();
+            if (dr == DialogResult.OK)
+            {
+                var folder = this.folderBrowserDialog1.SelectedPath;
+                if (!string.IsNullOrWhiteSpace(folder))
+                {
+                    var dir = new System.IO.DirectoryInfo(folder);
+                    foreach (var item in dir.EnumerateFiles())
+                    {
+                        if (item.Extension == ".pdf")
+                        {
+                            AppendFile(item.FullName, item.Name);
+                        }
+                    }
+                }
+            }
+            
+        }
+
+        private void btnChooseFile_Click(object sender, EventArgs e)
+        {
+            var dr = this.openFileDialog1.ShowDialog();
+            if (dr == DialogResult.OK)
+            {
+                var file = new System.IO.FileInfo(this.openFileDialog1.FileName);
+                AppendFile(file.FullName, file.Name);
+            }
+        }
+
+        private void btnExecute_Click(object sender, EventArgs e)
+        {
+            if (lstPDFFiles.Count == 0)
+            {
+                return;
+            }
+            foreach (var item in lstPDFFiles)
+            {
+                var fi = new FileInfo(item);
+                removePagesFromPdf(item, System.IO.Path.Combine(fi.DirectoryName, string.Format("{0}-trim.pdf", Path.GetFileNameWithoutExtension(item))));
+            }
+        }
+
+        //public static void ExtractPagesFromPdf(string inputFile, string outputFile, int start, int end)
+        //{
+        //    PdfReader inputPdf = new PdfReader(inputFile);
+        //    PdfDocument docIn = new PdfDocument(inputPdf);
+
+        //    PdfWriter outputWriter = new PdfWriter(outputFile);
+        //    PdfDocument docOut = new PdfDocument(outputWriter);
+
+        //    // retrieve the total number of pages
+        //    int pageCount = docIn.GetNumberOfPages();
+
+        //    if (end < start || end > pageCount)
+        //    {
+        //        end = pageCount;
+        //    }
+
+        //    var merge = new PdfMerger(docOut);
+
+        //    merge.Merge(docIn, start, end);
+
+        //    merge.Close();
+        //}
+
+        public void removePagesFromPdf(String sourceFile, String destinationFile)
+        {
+            try
+            {
+                //Used to pull individual pages from our source
+                PdfReader reader = new PdfReader(sourceFile);
+                if (reader.NumberOfPages < 2)
+                {
+                    Log(string.Format(" 文件 {0}页数=1，不删除", sourceFile));
+                    return;
+                }
+                //Create our destination file
+                using (FileStream fs = new FileStream(destinationFile, FileMode.Create, FileAccess.Write, FileShare.None))
+                {
+                    using (Document doc = new Document())
+                    {
+                        using (PdfWriter w = PdfWriter.GetInstance(doc, fs))
+                        {
+                            //Open the desitination for writing
+                            doc.Open();
+                            for (int i = 1; i < reader.NumberOfPages; i++)
+                            {
+                                //Add a new blank page to destination document
+                                doc.NewPage();
+                                //Extract the given page from our reader and add it directly to the destination PDF
+
+                                PdfContentByte cb = w.DirectContent;
+                                PdfImportedPage pageImport = w.GetImportedPage(reader, i);
+                                int rot = reader.GetPageRotation(i);
+                                //if (rot == 90)
+                                //    cb.AddTemplate(pageImport, 0, -1.0F, 1.0F, 0, 0, reader.GetPageSizeWithRotation(i).Height);
+                                //else if (rot == 270)
+                                //    cb.AddTemplate(pageImport, 0, -1.0F, 1.0F, 0, 0, reader.GetPageSizeWithRotation(i).Height);
+                                //else
+                                //    cb.AddTemplate(pageImport, 1.0F, 0, 0, 1.0F, 0, 0);
+                                switch (rot)
+                                {
+                                    case 0:
+                                        cb.AddTemplate(pageImport, 1f, 0, 0, 1f, 0, 0);
+                                        break;
+
+                                    case 90:
+                                        cb.AddTemplate(pageImport, 0, -1f, 1f, 0, 0, reader.GetPageSizeWithRotation(i).Height);
+                                        break;
+
+                                    case 180:
+                                        cb.AddTemplate(pageImport, -1f, 0, 0, -1f, reader.GetPageSizeWithRotation(i).Width, reader.GetPageSizeWithRotation(i).Height);
+                                        break;
+
+                                    case 270:
+                                        cb.AddTemplate(pageImport, 0, 1f, -1f, 0, reader.GetPageSizeWithRotation(i).Width, 0);
+                                        break;
+
+                                    default:
+                                        throw new InvalidOperationException(string.Format("Unexpected page rotation: [{0}].", rot));
+                                }
+                                //w.DirectContent.AddTemplate(w.GetImportedPage(r, i), 0, 0);
+                            }
+
+                            //Close our document
+                            doc.Close();
+                            Log(string.Format(" 文件 {0} 删除成功", sourceFile));
+                        }
+                    }
+                }
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                Log("发生未知错误：" + ex.ToString());
+            }
+            
+        }
+
+        private void Log(string msg)
+        {
+            txtLog.AppendText(string.Format(" {0}\r\n", msg));
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            this.lstPDFFiles.Clear();
+            this.lstFiles.Items.Clear();
+            this.txtLog.Text = "";
+        }
+    }
+}
